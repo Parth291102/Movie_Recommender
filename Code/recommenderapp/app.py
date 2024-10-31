dca3c31a9f65ad17143f2314d87f3e1c6ae55f95
Mohit Hosakatte Niranjana Murthy <mohithn7@gmail.com>
2024-10-31 23:10:54 +0000

    Updated app.py file

--- Code/recommenderapp/app.py
+++ Code/recommenderapp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
@@ -9,13 +9,7 @@ import csv
 import time
 import requests
 from datetime import datetime
-
-
-sys.path.append("../../")
-from Code.prediction_scripts.item_based import recommendForNewUser
 from search import Search
-
-import requests
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -38,11 +32,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    
+
 class Recommendation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -52,23 +46,18 @@ class Recommendation(db.Model):
     def __repr__(self):
         return f'<Recommendation {self.movie_title}>'
 
-
-# Replace 'YOUR_API_KEY' with your actual OMDB API key
 OMDB_API_KEY = 'b726fa05'
 
 def get_movie_info(title):
-    index=len(title)-6
-    url = f"http://www.omdbapi.com/?t={title[0:index]}&apikey={OMDB_API_KEY}"
-    print(url)
+    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
     response = requests.get(url)
     if response.status_code == 200:
-        res=response.json()
-        if(res['Response'] == "True"):
+        res = response.json()
+        if res['Response'] == "True":
             return res
-        else:  
-            return { 'Title': title, 'imdbRating':"N/A", 'Genre':'N/A',"Poster":"https://www.creativefabrica.com/wp-content/uploads/2020/12/29/Line-Corrupted-File-Icon-Office-Graphics-7428407-1.jpg"}
-    else:
-        return  { 'Title': title, 'imdbRating':"N/A",'Genre':'N/A', "Poster":"https://www.creativefabrica.com/wp-content/uploads/2020/12/29/Line-Corrupted-File-Icon-Office-Graphics-7428407-1.jpg"}
+        else:
+            return { 'Title': title, 'imdbRating': "N/A", 'Genre': 'N/A', "Poster": "https://via.placeholder.com/150" }
+    return { 'Title': title, 'imdbRating': "N/A", 'Genre': 'N/A', "Poster": "https://via.placeholder.com/150" }
 
 @app.route("/")
 def landing_page():
@@ -99,7 +88,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()  # 'user' is now defined here
+        user = User.query.filter_by(username=username).first()
 
         if user is None or not user.check_password(password):
             error = 'Invalid username or password'
@@ -107,10 +96,7 @@ def login():
             login_user(user)
             return redirect(url_for('landing_page'))
 
-    # If we reach this point without returning, 'user' was not assigned due to a POST
-    # Or there was an error in login, handle accordingly
     return render_template('login.html', error=error)
-
 
 @app.route('/logout')
 @login_required
@@ -118,52 +104,29 @@ def logout():
     logout_user()
     return redirect(url_for('landing_page'))
 
-
-
 @app.route("/predict", methods=["POST"])
-# def predict():
-#     data = json.loads(request.data)  # contains movies
-#     data1 = data["movie_list"]
-#     training_data = []
-#     for movie in data1:
-#         movie_with_rating = {"title": movie, "rating": 5.0}
-#         training_data.append(movie_with_rating)
-#     recommendations = recommendForNewUser(movie_with_rating)
-    
-#     for movie in data1:    
-#         movie_info = get_movie_info(movie)
-#         if movie_info:
-#             movie_with_rating["title"]=movie
-#             movie_with_rating["rating"]=movie_info["imdbRating"]
-    
-#     recommendations = recommendations[:10]
-#     resp = {"recommendations": recommendations}
-#     return resp
 def predict():
-    data = json.loads(request.data)  # contains movies
+    data = json.loads(request.data)
     data1 = data["movie_list"]
-    training_data = []
-    for movie in data1:
-        movie_with_rating = {"title": movie, "rating": 5.0}
-        training_data.append(movie_with_rating)
-    recommendations = recommendForNewUser(training_data)
-    recommendations = recommendations[:10]
+    training_data = [{"title": movie, "rating": 5.0} for movie in data1]
+
+    recommendations = recommendForNewUser(training_data)[:10]
+    movie_with_rating = {}
 
     for movie in recommendations:
         movie_info = get_movie_info(movie)
-        # print(movie_info['imdbRating'])
         if movie_info:
-            movie_with_rating[movie+"-r"]=movie_info['imdbRating']
-            movie_with_rating[movie+"-g"]=movie_info['Genre']
-            movie_with_rating[movie+"-p"]=movie_info['Poster']
+            movie_with_rating[movie + "-r"] = movie_info['imdbRating']
+            movie_with_rating[movie + "-g"] = movie_info['Genre']
+            movie_with_rating[movie + "-p"] = movie_info['Poster']
         
         new_recommendation = Recommendation(user_id=current_user.id, movie_title=movie)
         db.session.add(new_recommendation)
-    
+
     db.session.commit()
 
-    resp = {"recommendations": recommendations, "rating":movie_with_rating}
-    return resp
+    resp = {"recommendations": recommendations, "rating": movie_with_rating}
+    return jsonify(resp)
 
 @app.route("/history")
 @login_required
@@ -171,23 +134,20 @@ def history():
     recommendations = Recommendation.query.filter_by(user_id=current_user.id).all()
     return render_template('history.html', recommendations=recommendations)
 
-
 @app.route("/search", methods=["POST"])
 def search():
-    term = request.form["q"]
+    term = request.form.get("q", "")
     search = Search()
     filtered_dict = search.resultsTop10(term)
-    resp = jsonify(filtered_dict)
-    resp.status_code = 200
-    return resp
+    return jsonify(filtered_dict)
 
 @app.route("/feedback", methods=["POST"])
 def feedback():
     data = json.loads(request.data)
     with open(f"experiment_results/feedback_{int(time.time())}.csv", "w") as f:
-        for key in data.keys():
-            f.write(f"{key} - {data[key]}\n")
-    return data
+        for key, value in data.items():
+            f.write(f"{key} - {value}\n")
+    return jsonify(data)
 
 @app.route("/success")
 def success():
