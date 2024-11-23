c7cf4ca503d1d90cb2279223271a3d5a3a6542c5
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:05:57 -0500

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -83,3 +83,9 @@ def test_login_page_ui_elements():
     assert st.text_input.called_with("Password")
     assert st.button.called_with("Login")
             assert st.error.called  # Expect an error message for empty fields
+
+def test_successful_login():
+    users = {"valid_user": {"email": "valid@example.com", "password": hash_password("valid_password")}}
+    st.session_state['logged_in'] = False
+    validate_login("valid_user", "valid_password", users)
+    assert st.session_state['logged_in'] is True
