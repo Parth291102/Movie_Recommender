10ef55f5ff9d950c4668c285e489d8079592d798
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 17:55:01 -0500

    Delete test/test_no_recommendations.py

--- test/test_no_recommendations.py
+++
@@ -1,5 +0,0 @@
-def test_no_recommendations():
-    displayed.clear()
-    movies, posters = preprocess.recommend(mock_df, "Unknown Movie", "sample.pkl")
-    assert len(movies) == 0
-    assert st.text.called_with("No recommendations available.")
