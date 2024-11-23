535ed2c93bf3140f2236b769c517f3cab53d43f8
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:02:27 -0500

    Delete test/test_login_page_ui_elements.py

--- test/test_login_page_ui_elements.py
+++
@@ -1,5 +0,0 @@
-def test_login_page_ui_elements():
-    login_page()
-    assert st.text_input.called_with("Username")
-    assert st.text_input.called_with("Password")
-    assert st.button.called_with("Login")
