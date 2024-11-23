b9709dd14618afaf2b1f445fb2a2000d336e87ae
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:14:09 -0500

    Delete test/test_display_movie_details.py

--- test/test_display_movie_details.py
+++
@@ -1,4 +0,0 @@
-def test_display_movie_details(mocker):
-    mocker.patch("preprocess.get_details", return_value=["Poster", "Overview", ["Genre1", "Genre2"]])
-    display_movie_details("Sample Movie")
-    assert st.image.called_once_with("Poster")
