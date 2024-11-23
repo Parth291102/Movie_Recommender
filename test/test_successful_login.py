3832ffbbba1024bf0b918d1a07a055b1d9b7e818
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:05:31 -0500

    Delete test/test_successful_login.py

--- test/test_successful_login.py
+++
@@ -1,5 +0,0 @@
-def test_successful_login():
-    users = {"valid_user": {"email": "valid@example.com", "password": hash_password("valid_password")}}
-    st.session_state['logged_in'] = False
-    validate_login("valid_user", "valid_password", users)
-    assert st.session_state['logged_in'] is True
