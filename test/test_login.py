ee1a43b8576f004fb319f66c8a83731d53376c65
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:03:00 -0500

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -76,4 +76,10 @@ def test_empty_login_input():
     password = ""
     with pytest.raises(Exception, match="All fields are required."):
         validate_login(username, password)
+
+def test_login_page_ui_elements():
+    login_page()
+    assert st.text_input.called_with("Username")
+    assert st.text_input.called_with("Password")
+    assert st.button.called_with("Login")
             assert st.error.called  # Expect an error message for empty fields
