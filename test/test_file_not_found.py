91d79a0b16c26c561f84619f3ccd14e8dbd265fe
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 18:06:44 -0500

    Delete test/test_file_not_found.py

--- test/test_file_not_found.py
+++
@@ -1,4 +0,0 @@
-def test_file_not_found(mocker):
-    mocker.patch("builtins.open", side_effect=FileNotFoundError)
-    with pytest.raises(FileNotFoundError, match="File not found"):
-        load_users()
