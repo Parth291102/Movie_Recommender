23163a82d9a24dd7b6de53a477f4a4124b2771b3
Mrudanihada <109239871+Mrudanihada@users.noreply.github.com>
2024-11-23 17:53:39 -0500

    Delete test/test_recommended_tags.py

--- test/test_recommended_tags.py
+++
@@ -1,6 +0,0 @@
-def test_recommendation_tags(mocker):
-    mocker.patch("preprocess.recommend", return_value=(["Movie1", "Movie2"], ["Poster1", "Poster2"]))
-    displayed.clear()
-    movies, posters = preprocess.recommend(mock_df, "Sample Movie", "sample.pkl")
-    assert len(set(movies)) == 2
-    assert movies == ["Movie1", "Movie2"]
