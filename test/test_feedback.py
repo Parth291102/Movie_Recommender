e977b12f2d8f7f2546b9dfa99c377e08daaa925e
Jk42github <160773137+Jk42github@users.noreply.github.com>
2024-11-24 01:30:38 +0000

    test case added

--- test/test_feedback.py
+++ test/test_feedback.py
@@ -52,3 +52,10 @@ def test_dislike_movie_from_csv():
         movie_title = movie_data.iloc[1]["title"]  # Second movie in the dataset
         update_feedback(movie_title, "dislike")
         assert st.session_state["feedback"][movie_title] == "dislike"
+
+def test_like_movie_from_csv():
+    with patch("main.st.session_state", {"feedback": {}}):
+        movie_data = pd.read_csv("tmdb_5000_movies.csv")  
+        movie_title = movie_data.iloc[0]["title"]  
+        update_feedback(movie_title, "like")
+        assert st.session_state["feedback"][movie_title] == "like"        
